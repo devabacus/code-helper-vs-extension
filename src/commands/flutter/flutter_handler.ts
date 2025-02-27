dc69d61c36d8878c632e855eae07de80c66fe7e9
mrfrolk <ekaterita20@gmail.com>
2025-02-27 18:54:01 +0500

    1

--- src/commands/flutter/flutter_handler.ts
+++ src/commands/flutter/flutter_handler.ts
@@ -1,22 +1,7 @@
 import { Uri, window, workspace } from "vscode";
 import { createFlutterPackage } from "./create_package";
 
-/**
- * Функции-заглушки, которые будут заменены на реальные реализации.
- */
-// async function createFlutterPackage() {
-//     window.showInformationMessage('Создание Flutter пакета...');
-//     // Здесь будет логика для выполнения терминальных команд
-// }
 
-async function someOtherFunction() {
-    window.showInformationMessage('Запуск другой функции...');
-    // Другая логика
-}
-
-/**
- * Обработчик команд, который вызывает нужную функцию в зависимости от выбора пользователя.
- */
 export async function flutterHandler() {
     const options: { [key: string]: () => Promise<void> } = {
         'Создать Flutter пакет': createFlutterPackage,
