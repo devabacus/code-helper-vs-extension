35d930c5cc7d5305adffc7a32d11c8f697862bec
mrfrolk <ekaterita20@gmail.com>
2025-02-28 05:44:26 +0500

    1

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,14 @@
 import {
 	commands,
-	ExtensionContext,
-	languages,
-	window,
-	workspace,
+	ExtensionContext
 } from "vscode";
 import {
-	snippetGenerate, addFolders, writeToTerminal, registerCodeActions,
-	selectAndAddPowerShellScript, 
-}
-	from "./commands";
+	addFolders,
+	registerCodeActions,
+	snippetGenerate,
+	writeToTerminal
+} from "./commands";
 import { flutterHandler } from "./commands/flutter/flutter_handler";
-import { startFlutterApp } from "./commands/flutter/start_flutter_app";
-import { snippetGenerateWithWrapper } from "./commands/snippet_generator/snippet_generate2";
 
 
 
@@ -23,7 +19,6 @@ export function activate(context: ExtensionContext) {
 		commands.registerCommand('code-helper.snippet_generate', snippetGenerate),
 		commands.registerCommand('code-helper.add_folders', addFolders),
 		commands.registerCommand('code-helper.terminal', writeToTerminal),
-		// commands.registerCommand("code-helper.startApp", startFlutterApp),
 		commands.registerCommand("code-helper.myFlutter", flutterHandler)
 	);
 }
