5edce1fac50b3e05859caece2f650425eac70edc
mrfrolk <ekaterita20@gmail.com>
2025-03-18 15:07:47 +0500

    1

--- src/commands/flutter/handle_work/start_app_fix.ts
+++ src/commands/flutter/handle_work/start_app_fix.ts
@@ -5,7 +5,7 @@ import { fixAndroidNDKVersion } from './fix_android_ndk_version';
 
 export function startAppFix(rootPath: string) {
     const filePath = path.join(rootPath, "test", "widget_test.dart");
-    
+
     fs.unlinkSync(filePath);
-    fixAndroidNDKVersion();
+    // fixAndroidNDKVersion();
 }
