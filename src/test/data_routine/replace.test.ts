71ebb531d8ce0aa08b3f31a84debac64d4bd5015
mrfrolk <ekaterita20@gmail.com>
2025-03-29 18:07:34 +0500

    1

--- src/test/data_routine/replace.test.ts
+++ src/test/data_routine/replace.test.ts
@@ -8,15 +8,17 @@ suite('app database', () => {
         
         const simplestCont = `@DriftDatabase(tables: [])`;
         const simplestRes = `@DriftDatabase(tables: [CategoryTable])`;
-        const simplestRes2 = `@DriftDatabase(tables: [CategoryTable,TagTable])`;
+        const simplestRes2 = `@DriftDatabase(tables: [CategoryTable, TagTable])`;
 
-        // const newContent = simplestCont.replace(/tables: \[(.*)\]/g, 'tables: [$1,CategoryTable]');
-
-        const groupReplacer = (content: string, RegExp, newTableName: string) => simplestCont.replace(/tables: \[(.*)\]/g, (match,p1)=>{
+        const groupReplacer = (content: string, regex: RegExp, newTableName: string) => content.replace(regex, (match,p1)=>{
             const trimmedCont = p1.trim();
             return `tables: [${trimmedCont? trimmedCont + ', ':''}${newTableName}]`;
         });
         assert.strictEqual(groupReplacer(simplestCont, /tables: \[(.*)\]/g, 'CategoryTable'), simplestRes);
-        // assert.strictEqual(groupReplacer('TagTable'), simplestRes2);
+        
+        assert.strictEqual(groupReplacer(simplestRes, /tables: \[(.*)\]/g, 'TagTable'), simplestRes2);
+
+
+        
     });
 });
