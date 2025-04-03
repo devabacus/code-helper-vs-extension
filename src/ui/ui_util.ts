4dad47605a26f642fa96810c3044c339c36b7c8d
mrfrolk <ekaterita20@gmail.com>
2025-04-03 15:48:31 +0500

    откатился, сейчас норм

---
+++ src/ui/ui_util.ts
@@ -0,0 +1,62 @@
+import { Position, Range, window, workspace } from "vscode";
+import { createFolders } from "../utils";
+
+// Открываем файл в VS Code
+export function openTextDocument(destinationPath: string) {
+    workspace.openTextDocument(destinationPath).then((doc) => {
+        window.showTextDocument(doc);
+    });
+}
+
+
+export async function askPickOption(placeHolder: string, folderOptions: Record<string, string[]>): Promise<string | undefined> {
+    const selectedOption = await window.showQuickPick(Object.keys(folderOptions), {
+        placeHolder: placeHolder
+    });
+    return selectedOption || undefined;
+}
+
+//Добавляем выбранный набор папок в проект
+export async function addPickedFoldersSet(folderOptions: Record<string, string[]>) {
+
+    const selectedOption = await askPickOption("Выберите структуру папок", folderOptions);
+
+    if (selectedOption) {
+        const selectedFolders = folderOptions[selectedOption];
+        await createFolders(selectedFolders);
+    }
+}
+
+export function getDocText():string {
+    const doc = window.activeTextEditor;
+    return doc!.document.getText();
+}
+
+
+
+
+export function getCurrectSelect():string {
+    const editor = window.activeTextEditor;
+    const document = editor?.document;
+    const selection = editor?.selection;
+    return document!.getText(selection);
+}
+
+export function getCurrentLineText() {
+    const editor = window.activeTextEditor;
+    const cursor = editor!.selection.active;
+    const line = cursor.line;
+    const lineRange = new Range(new Position(line, 0), new Position(line+1, 0));
+    return  editor?.document.getText(lineRange);
+}
+
+
+        
+export async function confirmDialog(askMsg: string, positive: string, negative: string): Promise<boolean> {
+
+    const answer = await window.showInformationMessage(askMsg, positive, negative);
+
+    if (answer) { return true; }
+
+    return false;
+}
